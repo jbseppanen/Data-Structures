3e2e63bffe3eb15dc56657e494f35a9e8ff84343
Jacob Seppanen <jbseppanen@gmail.com>
2019-05-06 11:23:18 -0700

    Implement queues using a linked list.

--- queue/queue.py
+++ queue/queue.py
@@ -1,4 +1,4 @@
-class Queue:
+class QueueArr:
     def __init__(self):
         self.size = 0
         # what data structure should we
@@ -13,6 +13,30 @@ class Queue:
         if self.size > 0:
             self.size -= 1
             return self.storage.pop(0)
+        return None
+
+    def len(self):
+        return self.size
+
+
+from linked_list import LinkedList
+
+
+class Queue:
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = LinkedList()
+
+    def enqueue(self, item):
+        self.storage.add_to_tail(item)
+        self.size += 1
+
+    def dequeue(self):
+        if self.size > 0:
+            self.size -= 1
+            return self.storage.remove_head()
         return None
 
     def len(self):
